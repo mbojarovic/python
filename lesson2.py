21e7ce8596fb8c5ec5b932277f5815d3b0b01b11
Marek <m.bojarovic@gmail.com>
2023-04-10 18:41:36 +0300

    Задача 2-3

--- lesson2.py
+++ lesson2.py
@@ -5,7 +5,26 @@
 def fact(n):
     array = []
     res = 1
-    for el in range(1, n+1):
+    for el in range(1, n + 1):
         res = res * el
         array.append(res)
     print(f"N = {n} -> {array}", end=" ")
+
+
+# Задача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
+
+
+# Задача 3. Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй
+# «one» «onetwonine» - o – 2, n – 3, e – 2
+
+
+def func(first, second):
+    for i in first:
+        if i in second:
+            res = second.count(i)
+            print(f"{i} - {res}", end=" ")
+# func("one", "onetwonine")
+
+
+
+
