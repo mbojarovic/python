b023e6e98b899100d580d029d4820daaf48894ea
Marek <m.bojarovic@gmail.com>
2023-04-18 11:19:52 +0300

    Задача 4-1

---
+++ lesson4.py
@@ -0,0 +1,20 @@
+# Задача 1. Дано натуральное число N. Напишите метод,
+# который вернёт список простых множителей числа N и количество этих множителей.
+# 60 -> 2, 2, 3, 5
+import math
+
+
+def prime_factors(num):
+    print(f"{num} ->", end= " ")
+    while num % 2 == 0:
+        print(2, end=" ")
+        num = num / 2
+
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+
+        # while i divides n , print i ad divide n
+        while num % i == 0:
+            print(i, end=" ")
+            num = num / i
+    if num > 2:
+        print(int(num), end=" ")
